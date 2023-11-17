249d0f7c23e5e08d538539e0e3df1d025a08916e
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2023-11-18 01:45:53 +0300

    убирает черновые примеры

---
+++ dist/client/main.e19273a9af195618616c.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./src/shared/Header/NavBlock/navblock.css":
+/*!*************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-1!./src/shared/Header/NavBlock/navblock.css ***!
+  \*************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// Imports\nvar ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../../../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\nexports = ___CSS_LOADER_API_IMPORT___(false);\n// Module\nexports.push([module.i, \".navblock__nav--1FBsv {\\r\\n    flex-grow  : 1;\\r\\n    display    : flex;\\r\\n    align-items: center;\\r\\n}\\r\\n\\r\\n.navblock__list--rowDf {\\r\\n    display  : flex;\\r\\n    flex-wrap: wrap;\\r\\n}\\r\\n\\r\\n.navblock__item--2VnnI {\\r\\n    margin-bottom: 12px;\\r\\n}\\r\\n\\r\\n.navblock__item--2VnnI:last-child {\\r\\n    pointer-events: none;\\r\\n    opacity: 0;\\r\\n}\\r\\n\\r\\n\\r\\n.navblock__item--2VnnI a {\\r\\n    min-width    : 40px;\\r\\n    padding      : 10px 15px;\\r\\n    border       : 2px solid tomato;\\r\\n    border-radius: 5px;\\r\\n    font-size    : 20px;\\r\\n    line-height  : 24px;\\r\\n    color        : tomato;\\r\\n    margin-right : 20px;\\r\\n    transition   : opacity linear 0.3s;\\r\\n    display      : block;\\r\\n}\\r\\n\\r\\n.navblock__item--2VnnI a:hover {\\r\\n    opacity: 0.7;\\r\\n}\\r\\n\\r\\n\", \"\"]);\n// Exports\nexports.locals = {\n\t\"nav\": \"navblock__nav--1FBsv\",\n\t\"list\": \"navblock__list--rowDf\",\n\t\"item\": \"navblock__item--2VnnI\"\n};\nmodule.exports = exports;\n\n\n//# sourceURL=webpack:///./src/shared/Header/NavBlock/navblock.css?./node_modules/css-loader/dist/cjs.js??ref--5-1");
+
+/***/ })
+
+})
